6f4789999afd0e21156be68cc7e7c8bebadf743a
Daniel Holmlund <holmlund.dev@gmail.com>
2010-06-18 23:36:03 +0200

    Initial commits for the unit test visualization

--- t/TestConfig.pm
+++ t/TestConfig.pm
@@ -1,5 +1,9 @@
 package t::TestConfig;
 use Test::Base 0.51 -Base;
+
+use lib "../Religion-Bible-Regex-Config/lib";
+use lib "../Religion-Bible-Regex-Builder/lib";
+
 use Religion::Bible::Regex::Config;
 use Religion::Bible::Regex::Builder;
 
